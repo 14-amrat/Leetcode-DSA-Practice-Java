cf518e481d4f1dbfcbcf56b675b16f2ff1061448
Amrat Kumar Singh <tomaramrat888@gmail.com>
2026-07-31 13:19:59 +0530

    Time: 2 ms (88.74%), Space: 47.8 MB (81.72%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,14 +1,16 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        boolean[] dp = new boolean[nums.length];
-        dp[0] = true;
-        
+        int maxReach = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (!dp[i]) continue;  // Can't reach i
-            for (int j = 1; j <= nums[i] && i + j < nums.length; j++) {
-                dp[i + j] = true;
-            }
+            // Can't reach this index
+            if (i > maxReach) return false;
+            
+            // Update furthest reachable
+            maxReach = Math.max(maxReach, i + nums[i]);
+            
+            // Early exit
+            if (maxReach >= nums.length - 1) return true;
         }
-        return dp[nums.length - 1];
+        return true;
     }
 }
