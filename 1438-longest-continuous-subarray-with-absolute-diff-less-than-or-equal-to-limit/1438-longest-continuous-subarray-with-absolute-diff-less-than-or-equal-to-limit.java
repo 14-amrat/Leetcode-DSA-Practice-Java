318d2307ce12bd45aeebd65820f57bf9990a2e09
Amrat Kumar Singh <tomaramrat888@gmail.com>
2026-08-31 17:27:31 +0530

    Time: 165 ms (5.23%), Space: 91 MB (65.28%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -1,48 +1,65 @@
-import java.util.*;
+// import java.util.*;
 
-class Solution {
-    public int longestSubarray(int[] nums, int limit) {
+// class Solution {
+//     public int longestSubarray(int[] nums, int limit) {
         
-        Deque<Integer> maxDeque = new ArrayDeque<>();
-        Deque<Integer> minDeque = new ArrayDeque<>();
+//         Deque<Integer> maxDeque = new ArrayDeque<>();
+//         Deque<Integer> minDeque = new ArrayDeque<>();
 
-        int left = 0;
-        int maxLength = 0;
+//         int left = 0;
+//         int maxLength = 0;
 
-        for (int right = 0; right < nums.length; right++) {
+//         for (int right = 0; right < nums.length; right++) {
 
-            // Maintain decreasing deque for maximum
-            while (!maxDeque.isEmpty() &&
-                   nums[maxDeque.peekLast()] < nums[right]) {
-                maxDeque.pollLast();
-            }
-            maxDeque.offerLast(right);
+//             // Maintain decreasing deque for maximum
+//             while (!maxDeque.isEmpty() &&
+//                    nums[maxDeque.peekLast()] < nums[right]) {
+//                 maxDeque.pollLast();
+//             }
+//             maxDeque.offerLast(right);
 
-            // Maintain increasing deque for minimum
-            while (!minDeque.isEmpty() &&
-                   nums[minDeque.peekLast()] > nums[right]) {
-                minDeque.pollLast();
-            }
-            minDeque.offerLast(right);
+//             // Maintain increasing deque for minimum
+//             while (!minDeque.isEmpty() &&
+//                    nums[minDeque.peekLast()] > nums[right]) {
+//                 minDeque.pollLast();
+//             }
+//             minDeque.offerLast(right);
 
-            // Shrink window if invalid
-            while (nums[maxDeque.peekFirst()] - nums[minDeque.peekFirst()] > limit) {
+//             // Shrink window if invalid
+//             while (nums[maxDeque.peekFirst()] - nums[minDeque.peekFirst()] > limit) {
 
-                // Remove elements that are leaving the window
-                if (maxDeque.peekFirst() == left) {
-                    maxDeque.pollFirst();
-                }
+//                 // Remove elements that are leaving the window
+//                 if (maxDeque.peekFirst() == left) {
+//                     maxDeque.pollFirst();
+//                 }
 
-                if (minDeque.peekFirst() == left) {
-                    minDeque.pollFirst();
-                }
+//                 if (minDeque.peekFirst() == left) {
+//                     minDeque.pollFirst();
+//                 }
 
-                left++;
-            }
+//                 left++;
+//             }
 
-            maxLength = Math.max(maxLength, right - left + 1);
+//             maxLength = Math.max(maxLength, right - left + 1);
+//         }
+
+//         return maxLength;
+//     }
+// }
+
+class Solution{
+public int longestSubarray(int[] A, int limit) {
+    int i = 0, j;
+    TreeMap<Integer, Integer> m = new TreeMap<>();
+    for (j = 0; j < A.length; j++) {
+        m.put(A[j], 1 + m.getOrDefault(A[j], 0));
+        if (m.lastEntry().getKey() - m.firstEntry().getKey() > limit) {
+            m.put(A[i], m.get(A[i]) - 1);
+            if (m.get(A[i]) == 0)
+                m.remove(A[i]);
+            i++;
         }
-
-        return maxLength;
     }
+    return j - i;
+}
 }
